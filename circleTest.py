51bd1dd87703d4eef002ec07352914d5f7b3929b
sudo-style <87545795+sudo-style@users.noreply.github.com>
2023-05-16 16:54:59 -0400

    Update circleTest.py
    Made the graphics smoother, by using the setting the clock

--- circleTest.py
+++ circleTest.py
@@ -1,30 +1,38 @@
 import pygame
+from pygame.locals import *
 import math
+
 
 from Circle import Circle
 
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((900, 900))
 pygame.display.set_caption("Circle Test")
 
 # background
 background = pygame. Surface(screen. get_size ())
 background = background.convert ()
-background. fill ((0, 0, 255))
+background. fill ((0, 0, 30))
 
 # x,y of the top left corner with, height
 rect = (100, 100, 100, 100)
-color = (255, 255, 255, 128)  # white color
+white = (255, 255, 255, 128)  # white color
+red  = (255,0,0)
+yellow = (255,255,0)
+green = (0,255,0)
+
+FPS = 60
+
+
 width = 3
 
 # Main loop
 running = True
 t = 0
-deltaT = .0025
+deltaT = .0125
 WAVE_LENGTH = 400
-
-
 wave = []
+clock = pygame.time.Clock()
 
 while running:
     # set the screen blue
@@ -46,22 +54,24 @@ while running:
     # does the same, need to figure out how to make this recursive 
     c3 = Circle(x1,y1,25/2)    
     x2,y2 = c3.getXYCenterToRadius(start_angle_degrees+t*4)
-
-    wave = [y2] + wave
-    #print(x,y)
-
+    wave = [(x2,y2)] + wave
     # updates the display
-    #pygame.draw.circle(screen, color, (150,150), width)
+
+
     # first circle
-    pygame.draw.line(screen, color, (x,y), (c.centerX, c.centerY))
-    pygame.draw.line(screen, color, (x,y), (x1, y1))
-    pygame.draw.line(screen, color, (x1,y1), (x2, y2))
+    pygame.draw.line(screen, yellow, (x,y),  (c.centerX, c.centerY))
+    pygame.draw.line(screen, green,  (x,y),  (x1, y1))
+    pygame.draw.line(screen, yellow, (x1,y1),(x2, y2))
 
-
+    # draw a line from last orbit to the start of the wave
+    pygame.draw.line(screen, red, wave[0], (400,y2))
+    pygame.draw.line(screen, red, wave[0], (wave[0][0],400))
+    
     # draw all points in the wave
     for i in range(len(wave)):
-        point = wave[i]
-        pygame.draw.circle(screen, color, (200 + i,point), 1)
+        point = (wave[i][0],wave[i][1])
+        pygame.draw.circle(screen, white, (400 + i,point[1]), 1)
+        pygame.draw.circle(screen, white, (point[0],400 + i), 1)
 
     # draw the screen
     pygame.display.flip()
@@ -73,4 +83,6 @@ while running:
 
     if len(wave) > WAVE_LENGTH:
         wave.pop()
+    pygame.display.update()
+    clock.tick(FPS)
 pygame.quit()
